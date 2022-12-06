92f19c032f915a50d3598e1140a4dbc87c182f69
maxmcd <max.t.mcdonnell@gmail.com>
2022-12-06 11:22:19 -0300

    2022 6

---
+++ 2022/6/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/maxmcd/aoc"
+)
+
+func main() {
+	aoc.ReadInput(func(line string) {
+		// detectNDistinct(line, 4)
+		detectNDistinct(line, 14)
+	})
+}
+
+func detectNDistinct(line string, n int) {
+	for i := 0; i < len(line)-n; i++ {
+		track := make(map[string]struct{}, n)
+		for j := 0; j < n; j++ {
+			track[string(line[i+j])] = struct{}{}
+		}
+		if len(track) == n {
+			fmt.Println(i+n, track)
+			os.Exit(0)
+		}
+	}
+}
